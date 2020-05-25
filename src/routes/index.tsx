4cf6b837ff7b23e540ce1dabe48a91b494c6b072
unknown <fernandoselvino@gmail.com>
2020-05-24 23:47:58 -0300

    Configurando navegacao

---
+++ src/routes/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+
+const Auth = createStackNavigator();
+
+const AuthRoutes: React.FC = () => (
+  <Auth.Navigator
+    screenOptions={{
+      headerShown: false,
+      // headerTintColor: '#ffff',
+      // headerStyle: {
+      //   backgroundColor: '#7159c1',
+      // },
+      cardStyle: {
+        backgroundColor: '#312e38',
+      },
+    }}
+  >
+    <Auth.Screen name="SignIn" component={SignIn} />
+    <Auth.Screen name="SignUp" component={SignUp} />
+  </Auth.Navigator>
+);
+
+export default AuthRoutes;
