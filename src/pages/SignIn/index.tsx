59caabcc401579ed5a32c53186c731e2e79a332f
unknown <fernandoselvino@gmail.com>
2020-05-26 00:05:29 -0300

    Input & Button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Title } from './styles';
 import logoImg from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu Logon</Title>
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button
+        onPress={() => {
+          console.log('Deus');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
