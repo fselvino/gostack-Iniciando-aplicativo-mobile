107f7b5fc8890649ee3a9237a089653f16fb1565
unknown <fernandoselvino@gmail.com>
2020-05-25 22:47:25 -0300

    Importando fontes externas

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container } from './styles';
+import { Container, Title } from './styles';
 import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
+      <Title>Faça seu Logon</Title>
     </Container>
   );
 };
